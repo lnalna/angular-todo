88bdbb1e5f7a84d59c9e163ff80b535ca58d7a6e
nik <lnalna@yandex.ru>
2020-06-02 20:40:36 +0300

    таблица с задачами

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {Task} from 'src/app/model/Task';
 import {DataHandlerService} from '../../service/data-handler.service';
+import {Task} from 'src/app/model/Task';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-tasks',
@@ -9,16 +10,46 @@ import {DataHandlerService} from '../../service/data-handler.service';
 })
 export class TasksComponent implements OnInit {
 
+  // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
+  private displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  private dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
+
+
   tasks: Task[];
 
   constructor(private dataHandler: DataHandlerService) {
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.dataHandler.tasksSubject.subscribe(tasks => this.tasks = tasks);
+
+    // датасорс обязательно нужно создавать для таблицы, в него присваивается любой источник (БД, массивы, JSON и пр.)
+    this.dataSource = new MatTableDataSource();
+
+    this.refreshTable();
   }
+
 
   toggleTaskCompleted(task: Task) {
     task.completed = !task.completed;
+  }
+
+  // в зависимости от статуса задачи - вернуть цвет названия
+  private getPriorityColor(task: Task) {
+
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+
+    return '#fff';
+
+  }
+
+  // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
+  private refreshTable() {
+
+    this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
+
+
   }
 }
