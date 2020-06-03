7c6da43f9b4566fae244bc83dece4a9e9e10a442
nik <lnalna@yandex.ru>
2020-06-04 00:03:10 +0300

    Dao

---
+++ src/app/data/dao/impl/CategoryDAOArray.ts
@@ -0,0 +1,31 @@
+import {Category} from '../../../model/Category';
+import {CategoryDAO} from '../interface/CategoryDAO';
+import {Observable} from 'rxjs';
+
+export class CategoryDAOArray implements CategoryDAO {
+
+  add(T): Observable<Category> {
+    return undefined;
+  }
+
+  delete(id: number): Observable<Category> {
+    return undefined;
+  }
+
+  get(id: number): Observable<Category> {
+    return undefined;
+  }
+
+  getAll(): Observable<Category[]> {
+    return undefined;
+  }
+
+  search(title: string): Observable<Category[]> {
+    return undefined;
+  }
+
+  update(T): Observable<Category> {
+    return undefined;
+  }
+
+}
