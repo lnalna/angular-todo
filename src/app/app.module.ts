df47ab5797a74fe25345a61776e2d28103f1f2f2
nik <lnalna@yandex.ru>
2020-06-09 22:42:58 +0300

    урок 36: выбор даты во всплывающем окне

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,10 @@ import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import {MatOptionModule} from '@angular/material/core';
+import {MatNativeDateModule, MatOptionModule} from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
 import { ConfirmDialogComponent } from './dialog/confirm-dialog/confirm-dialog.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 
 @NgModule({
@@ -40,7 +41,9 @@ import { ConfirmDialogComponent } from './dialog/confirm-dialog/confirm-dialog.c
     MatButtonModule,
     MatIconModule,
     MatOptionModule,
-    MatSelectModule
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   entryComponents: [
--- src/app/dialog/edit-task-dialog/edit-task-dialog.component.ts
+++ src/app/dialog/edit-task-dialog/edit-task-dialog.component.ts
@@ -1,52 +1,59 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
-import {Task} from '../../model/Task';
 import {DataHandlerService} from '../../service/data-handler.service';
 import {Category} from '../../model/Category';
 import {Priority} from '../../model/Priority';
 import {ConfirmDialogComponent} from '../confirm-dialog/confirm-dialog.component';
+import {Task} from '../../model/Task';
+
 
 @Component({
   selector: 'app-edit-task-dialog',
   templateUrl: './edit-task-dialog.component.html',
   styleUrls: ['./edit-task-dialog.component.css']
 })
+
+// редактирование/создание задачи
 export class EditTaskDialogComponent implements OnInit {
-  categories: Category[];
-  priorities: Priority[];
-
-  dialogTitle: string; // заголовок окна
-  task: Task; // задача для редактирования/создания
-  // чтобы изменения не сказывались на самой задаче и можно было отменить изменения
-  // сохраняем все значения в отдельные переменные
-  // чтобы изменения не сказывались на самой задаче и можно было отменить изменения
-  tmpTitle: string;
-  tmpCategory: Category;
-  tmpPriority: Priority;
-
-  // сохраняем все значения в отдельные переменные
 
   constructor(
     private dialogRef: MatDialogRef<EditTaskDialogComponent>, // // для возможности работы с текущим диалог. окном
     @Inject(MAT_DIALOG_DATA) private data: [Task, string], // данные, которые передали в диалоговое окно
     private dataHandler: DataHandlerService, // ссылка на сервис для работы с данными
-    private dialog: MatDialog, // для открытия нового диалогового окна (из текущего) - например для подтверждения удаления
-
+    private dialog: MatDialog // для открытия нового диалогового окна (из текущего) - например для подтверждения удаления
   ) {
   }
+
+  categories: Category[];
+  priorities: Priority[];
+
+  dialogTitle: string; // заголовок окна
+  task: Task; // задача для редактирования/создания
+
+  // сохраняем все значения в отдельные переменные
+  // чтобы изменения не сказывались на самой задаче и можно было отменить изменения
+  tmpTitle: string;
+  tmpPriority: Priority;
+  tmpCategory: Category;
+  tmpDate: Date;
+
+
 
   ngOnInit() {
     this.task = this.data[0]; // задача для редактирования/создания
     this.dialogTitle = this.data[1]; // текст для диалогового окна
 
+
     // инициализация начальных значений (записывам в отдельные переменные
     // чтобы можно было отменить изменения, а то будут сразу записываться в задачу)
     this.tmpTitle = this.task.title;
-    this.tmpCategory = this.task.category;
     this.tmpPriority = this.task.priority;
+    this.tmpCategory = this.task.category;
+    this.tmpDate = this.task.date;
 
-    this.dataHandler.getAllCategories().subscribe(items => this.categories = items);
-    this.dataHandler.getAllPriorities().subscribe(items => this.priorities = items);
+    this.dataHandler.getAllCategories().subscribe( items => this.categories = items);
+    this.dataHandler.getAllPriorities().subscribe( items => this.priorities = items);
+
   }
 
   // нажали ОК
@@ -54,8 +61,10 @@ export class EditTaskDialogComponent implements OnInit {
 
     // считываем все значения для сохранения в поля задачи
     this.task.title = this.tmpTitle;
-    this.task.category = this.tmpCategory;
     this.task.priority = this.tmpPriority;
+    this.task.category = this.tmpCategory;
+    this.task.date = this.tmpDate;
+
 
     // передаем добавленную/измененную задачу в обработчик
     // что с ним будут делать - уже на задача этого компонента
@@ -97,5 +106,4 @@ export class EditTaskDialogComponent implements OnInit {
   activate() {
     this.dialogRef.close('activate');
   }
-
 }
