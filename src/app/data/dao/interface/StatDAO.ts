bd86ff36209e2399108bdae4e240a72a51302f0d
nik <lnalna@yandex.ru>
2020-07-08 20:15:55 +0300

    frontend3h: fix

--- src/app/data/dao/interface/StatDAO.ts
+++ src/app/data/dao/interface/StatDAO.ts
@@ -1,8 +1,7 @@
-import {CommonDAO} from './CommonDAO';
 import {Observable} from 'rxjs';
 import {Stat} from '../../../model/Stat';
 
 // общая статистика по всем задачам
-export interface StatDAO extends CommonDAO<Stat> {
+export interface StatDAO {
   getOverallStat(): Observable<Stat>;
 }
