8c0392a0b200ddf4982a7ca3568af952cc470384
nik <lnalna@yandex.ru>
2020-07-08 19:22:45 +0300

    frontend2: изменение DAO интерфейсов

---
+++ src/app/data/dao/search/SearchObjects.ts
@@ -0,0 +1,22 @@
+
+// все возможные параметры поиска категорий
+export class CategorySearchValues {
+    text: string = null;
+}
+
+// все возможные параметры поиска категорий
+export class TaskSearchValues {
+
+    // начальные значения по-умолчанию
+    title = '';
+    completed: number = null;
+    priorityId: number = null;
+    categoryId: number = null;
+    pageNumber = 0; // 1-я страница (значение по-умолчанию)
+    pageSize = 5; // сколько элементов на странице (значение по-умолчанию)
+
+    // сортировка
+    sortColumn = 'title';
+    sortDirection = 'asc';
+
+}
