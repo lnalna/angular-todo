1b9c28d91d5c965d1d54e84b51ebea2113f040ba
nik <lnalna@yandex.ru>
2020-07-09 20:10:40 +0300

    frontend7: поиск категорий

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Category} from './model/Category';
-import {Priority} from './model/Priority';
-import {Task} from './model/Task';
+import {CategorySearchValues} from './data/dao/search/SearchObjects';
+import {CategoryService} from './data/dao/impl/CategoryService';
 import {IntroService} from './service/intro.service';
 import {DeviceDetectorService} from 'ngx-device-detector';
-import {concatMap, map} from 'rxjs/operators';
-import {zip} from 'rxjs';
-import {CategoryService} from "./data/dao/impl/CategoryService";
 
 
 @Component({
@@ -29,6 +26,7 @@ export class AppComponent implements OnInit {
   // выбранная категория
   selectedCategory: Category = null; // null - значит будет выбрана категория "Все"
 
+
   // параметры бокового меню с категориями
   menuOpened: boolean; // открыть-закрыть
   menuMode: string; // тип выдвижения (поверх, с толканием и пр.)
@@ -38,6 +36,10 @@ export class AppComponent implements OnInit {
   // тип устройства
   isMobile: boolean;
   isTablet: boolean;
+
+  // параметры поисков
+  categorySearchValues = new CategorySearchValues(); // экземпляр можно создать тут же, т.к. не загружаем из cookies
+
 
 
   constructor(
@@ -53,6 +55,7 @@ export class AppComponent implements OnInit {
     this.showStat = true ? !this.isMobile : false; // если моб. устройство, то по-умолчанию не показывать статистику
 
     this.setMenuValues(); // установить настройки меню
+
 
   }
 
@@ -76,207 +79,52 @@ export class AppComponent implements OnInit {
 
 
   // добавление категории
-  addCategory(title: string): void {
-    // this.dataHandler.addCategory(title).subscribe(categories => {
-    //     this.onSearchCategory(this.searchCategoryText);
-    //     this.fillCategories();
-    //   }
-    // );
+  addCategory(category: Category) {
+    this.categoryService.add(category).subscribe(result => {
+
+      }
+    );
+  }
+
+  // удаление категории
+  deleteCategory(category: Category) {
+    this.categoryService.delete(category.id).subscribe(cat => {
+
+    });
+  }
+
+  // обновлении категории
+  updateCategory(category: Category) {
+    this.categoryService.update(category).subscribe(() => {
+
+    });
   }
 
 
   // заполняет категории и кол-во невыполненных задач по каждой из них (нужно для отображения категорий)
   fillAllCategories() {
+
     this.categoryService.findAll().subscribe(result => {
       this.categories = result;
     });
 
-    // if (this.categoryMap) {
-    //   this.categoryMap.clear();
-    // }
-
-    // this.categories = this.categories.sort((a, b) => a.title.localeCompare(b.title));
-
-    // для каждой категории посчитать кол-во невыполненных задач
-
-    // this.categories.forEach(cat => {
-    //   this.dataHandler.getUncompletedCountInCategory(cat).subscribe(count => this.categoryMap.set(cat, count));
-    // });
 
   }
 
   // поиск категории
-  searchCategory(title: string): void {
-
-    // this.searchCategoryText = title;
-
-    // this.dataHandler.searchCategories(title).subscribe(categories => {
-    //   this.categories = categories;
-    //   this.fillCategories();
-    // });
+  searchCategory(categorySearchValues: CategorySearchValues) {
+    this.categoryService.findCategories(categorySearchValues).subscribe(result => {
+      this.categories = result;
+    });
   }
 
 
   // изменение категории
   selectCategory(category: Category): void {
 
-    this.selectedCategory = category;
-
-    this.updateTasksAndStat();
-
-    if (this.isMobile) {
-      this.menuOpened = false; // закрываем боковое меню
-    }
 
   }
 
-
-  // удаление категории
-  deleteCategory(category: Category) {
-    // this.dataHandler.deleteCategory(category.id).subscribe(cat => {
-    //   this.selectedCategory = null; // открываем категорию "Все"
-    //   this.categoryMap.delete(cat); // не забыть удалить категорию из карты
-    //   this.onSearchCategory(this.searchCategoryText);
-    //   this.updateTasks();
-    // });
-  }
-
-  // обновлении категории
-  updateCategory(category: Category): void {
-    // this.dataHandler.updateCategory(category).subscribe(() => {
-    //   this.onSearchCategory(this.searchCategoryText);
-    // });
-  }
-
-  // обновление задачи
-  onUpdateTask(task: Task): void {
-
-    // this.dataHandler.updateTask(task).subscribe(() => {
-    //
-    //   this.fillCategories();
-    //
-    //   this.updateTasksAndStat();
-    // });
-
-  }
-
-
-  // удаление задачи
-  onDeleteTask(task: Task) {
-
-    // this.dataHandler.deleteTask(task.id).pipe(
-    //   concatMap(task => {
-    //       return this.dataHandler.getUncompletedCountInCategory(task.category).pipe(map(count => {
-    //         return ({t: task, count});
-    //       }));
-    //     }
-    //   )).subscribe(result => {
-    //
-    //   const t = result.t as Task;
-    //
-    //   // если указана категория - обновляем счетчик для соотв. категории
-    //   // чтобы не обновлять весь список - обновим точечно
-    //   if (t.category) {
-    //     this.categoryMap.set(t.category, result.count);
-    //   }
-    //
-    //   this.updateTasksAndStat();
-    //
-    // });
-
-
-  }
-
-
-  // поиск задач
-  onSearchTasks(searchString: string): void {
-    // this.searchTaskText = searchString;
-    // this.updateTasks();
-  }
-
-  // фильтрация задач по статусу (все, решенные, нерешенные)
-  onFilterTasksByStatus(status: boolean): void {
-    // this.statusFilter = status;
-    // this.updateTasks();
-  }
-
-  // фильтрация задач по приоритету
-  onFilterTasksByPriority(priority: Priority): void {
-    // this.priorityFilter = priority;
-    // this.updateTasks();
-  }
-
-  // обновить список задач
-  updateTasks(): void {
-    // this.dataHandler.searchTasks(
-    //   this.selectedCategory,
-    //   this.searchTaskText,
-    //   this.statusFilter,
-    //   this.priorityFilter
-    // ).subscribe((tasks: Task[]) => {
-    //   this.tasks = tasks;
-    // });
-  }
-
-
-  // добавление задачи
-  onAddTask(task: Task) {
-
-
-    // this.dataHandler.addTask(task).pipe(// сначала добавляем задачу
-    //   concatMap(task => { // используем добавленный task (concatMap - для последовательного выполнения)
-    //       // .. и считаем кол-во задач в категории с учетом добавленной задачи
-    //       return this.dataHandler.getUncompletedCountInCategory(task.category).pipe(map(count => {
-    //         return ({t: task, count}); // в итоге получаем массив с добавленной задачей и кол-вом задач для категории
-    //       }));
-    //     }
-    //   )).subscribe(result => {
-    //
-    //   const t = result.t as Task;
-    //
-    //   // если указана категория - обновляем счетчик для соотв. категории
-    //   // чтобы не обновлять весь список - обновим точечно
-    //   if (t.category) {
-    //     this.categoryMap.set(t.category, result.count);
-    //   }
-    //
-    //   this.updateTasksAndStat();
-    //
-    // });
-
-  }
-
-
-  // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
-  updateTasksAndStat(): void {
-
-    this.updateTasks(); // обновить список задач
-
-    // обновить переменные для статистики
-    this.updateStat();
-
-  }
-
-  // обновить статистику
-  updateStat(): void {
-    // zip(
-    //     //   this.dataHandler.getTotalCountInCategory(this.selectedCategory),
-    //     //   this.dataHandler.getCompletedCountInCategory(this.selectedCategory),
-    //     //   this.dataHandler.getUncompletedCountInCategory(this.selectedCategory),
-    //     //   this.dataHandler.getUncompletedTotalCount())
-    //     //
-    //     //   .subscribe(array => {
-    //     //     this.totalTasksCountInCategory = array[0];
-    //     //     this.completedCountInCategory = array[1];
-    //     //     this.uncompletedCountInCategory = array[2];
-    //     //     this.uncompletedTotalTasksCount = array[3]; // нужно для категории Все
-    //     //   });
-  }
-
-  // показать-скрыть статистику
-  toggleStat(showStat: boolean): void {
-    this.showStat = showStat;
-  }
 
   // если закрыли меню любым способом - ставим значение false
   onClosedMenu() {
--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,32 +1,58 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Category} from '../../model/Category';
-import {MatDialog} from '@angular/material/dialog';
 import {DeviceDetectorService} from 'ngx-device-detector';
-import {EditCategoryDialogComponent} from '../../dialog/edit-category-dialog/edit-category-dialog.component';
-import {OperType} from '../../dialog/OperType';
-
+import {MatDialog} from '@angular/material/dialog';
+import {CategorySearchValues} from "../../data/dao/search/SearchObjects";
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
+
+// "presentational component": отображает полученные данные и отправляет какие-либо действия обработчику
+// назначение - работа с категориями
+// класс не видит dataHandler, т.к. напрямую с ним не должен работать
 export class CategoriesComponent implements OnInit {
+
+  // компонент взаимодействует с "внешним миром" только через @Input() и @Output !!!
+
+  // принцип инкпсуляции и "слабой связи"
+  // (Low Coupling) из GRASP —
+  // General Responsibility Assignment Software Patterns (основные шаблоны распределения обязанностей в программном обеспечении)
+  // с помощью @Output() сигнализируем о том, что произошло событие выбора категории (кто будет это обрабатывать - компонент не знает)
+
+
+  // ----------------------- входящие параметры ----------------------------
+
+  // сеттеры используются для доп. функционала - чтобы при изменении значения вызывать нужные методы
+  // а так можно использовать и обычные переменныые
+
+  // выбранная категория для отображения
+
+  @Input('selectedCategory')
+  set setCategory(selectedCategory: Category) {
+    this.selectedCategory = selectedCategory;
+  }
 
   @Input('categories')
   set setCategories(categories: Category[]) {
-    this.categories = categories;
-    console.log(categories);
+    this.categories = categories; // категории для отображения
   }
 
-  @Input()
-  selectedCategory: Category;
+  @Input('categorySearchValues')
+  set setCategorySearchValues(categorySearchValues: CategorySearchValues) {
+    this.categorySearchValues = categorySearchValues;
+  }
 
-  // кол-во невыполненных задач всего
-  @Input()
-  uncompletedCountForCategoryAll: number;
+  // используется для категории Все
+  @Input('uncompletedCountForCategoryAll')
+  set uncompletedCount(uncompletedCountForCategoryAll: number) {
+    this.uncompletedCountForCategoryAll = uncompletedCountForCategoryAll;
+  }
 
 
+  // ----------------------- исходящие действия----------------------------
 
   // выбрали категорию из списка
   @Output()
@@ -42,110 +68,102 @@ export class CategoriesComponent implements OnInit {
 
   // добавили категорию
   @Output()
-  addCategory = new EventEmitter<string>(); // передаем только название новой категории
+  addCategory = new EventEmitter<Category>(); // передаем только название новой категории
 
   // поиск категории
   @Output()
-  searchCategory = new EventEmitter<string>(); // передаем строку для поиска
+  searchCategory = new EventEmitter<CategorySearchValues>(); // передаем строку для поиска
+
+  // -------------------------------------------------------------------------
 
 
-  isMobile: boolean;
-
-  categories: Category[];
-
+  selectedCategory; // если равно null - по-умолчанию будет выбираться категория 'Все' - задачи любой категории (и пустой в т.ч.)
 
   // для отображения иконки редактирования при наведении на категорию
   indexMouseMove: number;
-  searchCategoryTitle: string; // текущее значение для поиска категорий
-  isTablet: boolean;
+  showEditIconCategory: boolean; // показывать ли иконку редактирования категории
+
+  isMobile: boolean; // мобильное ли устройство
+
+  categories: Category[]; // категории для отображения
+
+  // параметры поиска категорий
+  categorySearchValues: CategorySearchValues;
+
+  // кол-во незавершенных задач для категории Все (для остальных категорий статис-ка подгружаются вместе с самой категорией)
+  uncompletedCountForCategoryAll: number;
+
+  filterTitle: string;
+
+  filterChanged: boolean; // были ли изменения в параметре поиска
 
   constructor(
     private dialog: MatDialog, // внедряем MatDialog, чтобы работать с диалоговыми окнами
-    private deviceService: DeviceDetectorService // для определения типа устройства
-
+    private deviceService: DeviceDetectorService
   ) {
     this.isMobile = deviceService.isMobile();
-    this.isTablet = deviceService.isTablet();
-
   }
 
-
-
-  // метод вызывается автоматически после инициализации компонента
   ngOnInit() {
-    // this.dataHandler.getAllCategories().subscribe(categories => this.categories = categories);
   }
 
-
-  showTasksByCategory(category: Category): void {
-
-    // если не изменилось значение, ничего не делать (чтобы лишний раз не делать запрос данных)
-    if (this.selectedCategory === category) {
-      return;
-    }
-
-    this.selectedCategory = category; // сохраняем выбранную категорию
-
-    // вызываем внешний обработчик и передаем туда выбранную категорию
-    this.selectCategory.emit(this.selectedCategory);
-  }
-
-  // сохраняет индекс записи категории, над который в данный момент проходит мышка (и там отображается иконка редактирования)
-  showEditIcon(index: number): void {
-    this.indexMouseMove = index;
-
-  }
-
-  // диалоговое окно для редактирования категории
-  openEditDialog(category: Category): void {
-    const dialogRef = this.dialog.open(EditCategoryDialogComponent, {
-      data: [category.title, 'Редактирование категории', OperType.EDIT],
-      width: '400px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-
-      if (result === 'delete') { // нажали удалить
-
-        this.deleteCategory.emit(category); // вызываем внешний обработчик
-
-        return;
-      }
-
-      if (result as string) { // нажали сохранить
-        category.title = result as string;
-
-        this.updateCategory.emit(category); // вызываем внешний обработчик
-        return;
-      }
-    });
-  }
 
   // диалоговое окно для добавления категории
-  openAddDialog(): void {
+  openAddDialog() {
 
-    const dialogRef = this.dialog.open(EditCategoryDialogComponent, {
-      data: ['', 'Добавление категории', OperType.ADD],
-      width: '400px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.addCategory.emit(result as string); // вызываем внешний обработчик
-      }
-    });
   }
+
+
+  // диалоговое окно для редактирования категории
+  openEditDialog(category: Category) {
+
+  }
+
 
   // поиск категории
-  search(): void {
+  search() {
 
+    this.filterChanged = false; // сбросить
 
-    if (this.searchCategoryTitle == null) {
+    if (!this.categorySearchValues) { // если объект с параметрами поиска непустой
       return;
     }
-    this.searchCategory.emit(this.searchCategoryTitle);
+
+    this.categorySearchValues.title = this.filterTitle;
+    this.searchCategory.emit(this.categorySearchValues);
 
   }
 
 
+  // выбираем категорию для отображения
+  showCategory(category: Category) {
+
+
+  }
+
+
+  // сохраняет индекс записи категории, над который в данный момент проходит мышка (и там отображается иконка редактирования)
+  showEditIcon(show: boolean, index: number) {
+    this.indexMouseMove = index;
+    this.showEditIconCategory = show;
+  }
+
+
+  clearAndSearch() {
+    this.filterTitle = null;
+    this.search();
+  }
+
+  // проверяет, были ли изменены какие-либо параметры поиска (по сравнению со старым значением)
+  checkFilterChanged() {
+
+    this.filterChanged = false;
+
+    if (this.filterTitle !== this.categorySearchValues.title){
+      this.filterChanged = true;
+    }
+
+    return this.filterChanged;
+
+  }
 }
